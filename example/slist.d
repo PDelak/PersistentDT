015baec75edd1880d6c8e5b660e1f3628b8f7b2c
PDelak <przemyslaw.delewski@gmail.com>
2016-10-20 22:34:00 +0200

    bsttree initial commit

--- example/slist.d
+++ example/slist.d
@@ -1,9 +1,23 @@
 import persistent.slist;
 import persistent.set;
 import persistent.map;
+import persistent.bsttree;
 import std.typecons;
 import std.stdio;
 import std.algorithm.searching;
+
+void visitTreeNode(T)(T node)
+{
+  if(!node) return;
+  writeln(node.key);
+  visitTreeNode(node.left);
+  visitTreeNode(node.right);
+}
+
+void visitTree(T)(PersistentBstTree!T tree)
+{
+  visitTreeNode(tree.treeRoot());
+}
 
 int main() 
 {
@@ -25,5 +39,13 @@ int main()
   PersistentMap!(int, int) map;
   auto newMap = map.add(tuple(3,3));
 
+
+  PersistentBstTree!int tree;
+
+  PersistentBstTree!int tree2 = tree.insert(10).insert(20).insert(5);
+  PersistentBstTree!int tree3 = tree2.insert(40);
+
+  visitTree(tree3);
+  visitTree(tree2);
   return 0;
 }
---
+++ persistent/bsttree.d
@@ -0,0 +1,51 @@
+module persistent.bsttree;
+
+import std.range;
+
+struct PersistentBstTree(T)
+{  
+  private struct node(T)
+  {
+    this(T v) { key = v; left = null; right = null; }
+    T key;
+    node* left;
+    node* right;
+  };
+
+  private node!T *root;
+
+  private alias node!T TreeNode;
+
+  private TreeNode* makeTreeNode(T)(T key, TreeNode* left, TreeNode* right)
+  {
+    TreeNode* newNode = new TreeNode(key);
+    newNode.left = left;
+    newNode.right = right;
+    return newNode;
+  }
+
+  private TreeNode* insertImpl(TreeNode* n, T key)
+  {
+    if(!n) return new TreeNode(key);
+   
+    if(key < n.key) return makeTreeNode(n.key, insert(n.left, key), n.right);
+
+    return makeTreeNode(n.key, n.left, insert(n.right, key));    
+  }
+
+  TreeNode* treeRoot() { return root; }
+
+  TreeNode* insert(TreeNode* n, T key)
+  {
+    return insertImpl(n, key);
+  }
+
+  PersistentBstTree!T insert(T key) {
+
+    PersistentBstTree!T tree;
+    tree.root = insert(root, key);
+    return tree;
+  
+  }
+
+};
